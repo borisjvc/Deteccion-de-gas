3303eab38f055a59848b1924f801b7e1101f9463
borisjvc <43859131+borisjvc@users.noreply.github.com>
2024-04-16 07:53:08 -0500

    websocket

--- websocket/websocket.py
+++ websocket/websocket.py
@@ -3,6 +3,7 @@ import websockets
 import mysql.connector
 import serial
 import json
+import time
 
 # Configuración de la base de datos
 db_config = {
@@ -13,7 +14,7 @@ db_config = {
 }
 
 # Configura el puerto serial según tu configuración de Arduino
-arduino_serial = "a"
+arduino_serial = serial.Serial('COM4', 9600, timeout=1)
 connected_clients = set()
 def update_config(tipo, valor):
     try:
@@ -21,6 +22,19 @@ def update_config(tipo, valor):
         cursor = conn.cursor()
         query = "UPDATE datos_sensor SET {} = %s ORDER BY id_sensorData DESC LIMIT 1".format(tipo)
         cursor.execute(query, (valor,))
+        conn.commit()
+    except mysql.connector.Error as e:
+        print(f"Error de base de datos: {e}")
+    finally:
+        cursor.close()
+        conn.close()
+
+def update_data(state):
+    try:
+        conn = mysql.connector.connect(**db_config)
+        cursor = conn.cursor()
+        query = "INSERT INTO datos_sensor (nivel_gas) VALUES (%s)"
+        cursor.execute(query, (state,))
         conn.commit()
     except mysql.connector.Error as e:
         print(f"Error de base de datos: {e}")
@@ -50,27 +64,32 @@ def get_sensor_data():
         cursor.close()
         conn.close()
 
+def arduino_data():
+    arduino_serial.write(b'get_state\n')  # Solicita el estado actual
+    time.sleep(0.1)  # Da tiempo al Arduino para responder
+    if arduino_serial.inWaiting() > 0:
+        state = arduino_serial.readline().decode().strip()
+        update_data(state)
+        return get_sensor_data()
+    return "0"  # Devuelve un estado predeterminado si no hay respuesta
 
 async def handle_arduino(websocket, path):
     connected_clients.add(websocket)
-    status = get_sensor_data()
+    status = arduino_data()
     await websocket.send(json.dumps(status))  # Enviar estado actual al cliente al conectarse
     async for message in websocket:
         try:
             data = json.loads(message)
             if 'alarmVolume' in data:
                 update_config(tipo='volumen', valor=data['alarmVolume'])
-                #arduino_serial.write(f"0 {data['alarmVolume']}".encode())
+                arduino_serial.write(f"0 {data['alarmVolume']}".encode())
             if 'threshold' in data:
                 update_config(tipo='umbral', valor=data['threshold'])
-                #arduino_serial.write(f"1 {data['threshold']}".encode())
+                arduino_serial.write(f"1 {data['threshold']}".encode())
             if 'alarmOff' in data:
                 update_config(tipo='alarma', valor=data['alarmOff'])
-                #arduino_serial.write(f"2 {data['alarmOff']}".encode())
-            if 'gas' in data:
-                update_config(tipo='nivel_gas', valor=data['gas'])
-                #arduino_serial.write(f"2 {data['alarmOff']}".encode())
-            status = get_sensor_data()
+                arduino_serial.write("alarma".encode())
+            status = arduino_data()
             print(connected_clients)
             for client in connected_clients:
                 await client.send(json.dumps(status))
